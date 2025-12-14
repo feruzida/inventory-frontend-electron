5cdbf2f33ff272795eeef40ea54d9d7ecb971530
Amir <149962379+blackamu@users.noreply.github.com>
2025-12-14 12:45:01 +0500

    Add files via upload

---
+++ Frontend/main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow, ipcMain } = require('electron');
+const net = require('net');
+
+let socket;
+let win;
+
+function createWindow() {
+  win = new BrowserWindow({
+    width: 1000,
+    height: 700,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+
+  win.loadFile('index.html');
+}
+
+app.whenReady().then(createWindow);
+
+ipcMain.on('connect-server', () => {
+  socket = new net.Socket();
+  socket.connect(5000, '127.0.0.1');
+
+  socket.on('data', (data) => {
+    win.webContents.send('server-response', data.toString());
+  });
+});
+
+ipcMain.on('send-data', (event, msg) => {
+  if (socket) socket.write(msg);
+});
